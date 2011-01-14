f7a262a2c2e2f59d88caa494bcee488d38941557
Ravi Kumar S <ravi.kumars@sasken.com>
2011-01-14 10:22:20 +0530

    Added bug id in change log and update copyright in tests directory

--- tests/src/transferuitests.h
+++ tests/src/transferuitests.h
@@ -1,3 +1,23 @@
+
+/*
+ * transfer-ui -- Handset UX Transfer user interface
+ * Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Contact: Jukka Tiihonen <jukka.tiihonen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 2.1, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef _TRANSFER_UI_TESTS
 #define _TRANSFER_UI_TESTS
 
