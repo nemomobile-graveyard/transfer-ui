04ada340c35a8ef909db15d7a0a00cbb2271fa98
divya <divya.subramanian@sasken.com>
2011-04-13 13:48:35 +0530

    Removed "\n" from the only two files in which it was there for the license statement

--- libtuiclient/src/mainpage.h
+++ libtuiclient/src/mainpage.h
@@ -4,7 +4,7 @@
  \brief File containing main page of documentation
  \author  Jukka Tiihonen <jukka.t.tiihonen@nokia.com>
  \copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
- \license GNU Lesser General Public License, version 2.1\n
+ \license GNU Lesser General Public License, version 2.1
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to 
  deal in the Software without restriction, including without limitation the
